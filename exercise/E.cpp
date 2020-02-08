e7075a7245eeb4fda3235bcbd71805a7f2bef121
tina <595101270@qq.com>
2020-02-08 15:26:10 +0800

    add manacher.cpp

--- exercise/E.cpp
+++ exercise/E.cpp
@@ -1,33 +1,7 @@
-//E-排序
-//2018/10/14    10:51
-#include <set>
-#include <cstdio>
 #include <iostream>
-#include <sstream>
 using namespace std;
-
-multiset<int> dict;//string集合
-
 int main(){
-    freopen("E_in.txt", "r", stdin);
-    string s;
-    int buf;
-    while(cin >> s){
-        dict.clear();
-        for (int i = 0; i < s.length(); i++){
-            if(s[i] == '5') s[i] = ' ';
-        }
-
-        stringstream ss(s);
-        while(ss >> buf){
-            dict.insert(buf);
-        }
-        multiset<int>::iterator it = dict.begin();
-        cout << *it++;
-        for(; it != dict.end(); it++){
-            cout << " " << *it;
-        }
-        printf("\n");
-    }
+    const int N = 1e5 + 10;
+    cout << N << endl;
     return 0;
 }
